70d2844c48b1462251e08c3b3058f53a5b44f8ba
Hardik Shah <hardiks2896ss@gmail.com>
2023-05-10 20:32:24 +0530

    Added Login Componenet and added to AppModule and app-routing with configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { AuthComponent } from './auth/auth.component';
 // added new routes
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'login', component: AuthComponent },
 ];
 
 @NgModule({
